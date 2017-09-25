a263f8931381a75ad7fc026592f2a1f4debad8ce
Paco Vu <paco_vu@yahoo.com>
2017-09-25 09:57:16 -0700

    Updated

--- Call Analytics/Credentials.swift
+++ Call Analytics/Credentials.swift
@@ -8,8 +8,6 @@
 
 struct Configs {
     let AppName = "CallAnalytics"
-    //static let AppKey = "Your-AppKey"
-    //static let AppSecret = "Your-AppSecret"
-    static let AppKey = "wP8ISPyTShOGhyGxPuWC4A"
-    static let AppSecret = "qad6qHnyTRKjpWETAXrf1ghPXbLFD3QKWuNnJiPUd6IQ"
+    static let AppKey = "Your-AppKey"
+    static let AppSecret = "Your-AppSecret"
 }
